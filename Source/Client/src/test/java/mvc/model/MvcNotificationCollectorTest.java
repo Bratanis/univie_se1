44442ce18929adac2f3afa78227b50979cc586f0
Ivan Bratanov <a12228892@unet.univie.ac.at>
2025-01-09 15:45:03 +0100

    Think: Business Rule: Technically interested users want to see the internals of the map generation, specifically the map validation displayed in the console; As of now notifications are collected but not displayed by the mvc View class

--- Source/Client/src/test/java/mvc/model/MvcNotificationCollectorTest.java
+++ Source/Client/src/test/java/mvc/model/MvcNotificationCollectorTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 class MvcNotificationCollectorTest {
 
+// Maybe make a mock view that will have a handlePropertyChange that will 
+// output the received string of the last notification (the change)
 	@Test
 	void test() {
 		fail("Not yet implemented");
